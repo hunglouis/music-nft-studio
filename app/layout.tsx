02ef47c9d6cdaea98f503cf9f8d7ae490d4b878c
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-03-20 05:50:47 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; // Đảm bảo bạn đã có file globals.css, nếu không hãy xóa dòng này
+
 
 const inter = Inter({ subsets: ["latin"] });
 
