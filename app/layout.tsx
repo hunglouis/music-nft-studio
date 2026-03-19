c5a983ff0ff389aaa53bcbc2a2a7dd69fc9253b3
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-03-19 19:01:41 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Music NFT Studio",
+  description: "NFT Music App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
