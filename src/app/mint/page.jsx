205ca325217477d40fa11eeb5e14fd6e278f0080
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-05-17 10:35:08 +0700

    fix: sua components thanh Components viet hoa

--- src/app/mint/page.jsx
+++ src/app/mint/page.jsx
@@ -1,4 +1,4 @@
-import MintIntoCollection from "../../components/MintIntoCollection";
+import MintIntoCollection from "../../Components/MintIntoCollection";
 
 export default function MintPage() {
   return <MintIntoCollection />;
